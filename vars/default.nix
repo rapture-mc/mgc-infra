d265a68c30f1231754315f52e89bce54c3899d27
Ben Harris <ben.r.harris@protonmail.com>
2025-01-09 14:32:31 +0930

    changing gitea to github

--- vars/default.nix
+++ vars/default.nix
@@ -1,5 +1,5 @@
 {
   networking = import ./networking.nix;
 
-  moduleSource = "git::https://gitea.megacorp.industries/megacorp/terraform-libvirt-module.git?ref=40acff807a0ffb1c0da741774c37ebeda90730b7";
+  moduleSource = "git::https://github.com/rapture-mc/terraform-libvirt-module.git?ref=40acff807a0ffb1c0da741774c37ebeda90730b7";
 }
